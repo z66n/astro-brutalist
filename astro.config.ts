5e9457b6ed5fdfa2a1b6c4818081726cf44d5391
l328949842 <l328949842@gmail.com>
2025-06-04 04:32:23 +0000

    Adding Cloudflare worker stuff for deployment

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
@@ -12,4 +14,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
